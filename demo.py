fb4eeafdca814bf67749ca02532a935f6a5cad43
andyyyds <34959818+andyyds-song@users.noreply.github.com>
2024-01-15 15:03:06 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 import time,sys
-username = sys.argv[1] 
-password = sys.argv[2]
+#username = sys.argv[1] 
+#password = sys.argv[2]
 def main():
     localtime = time.asctime( time.localtime(time.time()) )
     print("Hello, GitHub Actions!")
     print("运行时间为：",localtime)
-    print('账号：',username)
-    print('密码: ',password)
+    #print('账号：',username)
+    #print('密码: ',password)
 if __name__ == "__main__":
     main()
