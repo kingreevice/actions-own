dd66405947e49d669169eaac4fa71a5f66d97a78
andyyyds <34959818+andyyds-song@users.noreply.github.com>
2024-01-15 12:15:09 +0800

    Update dome.py

--- dome.py
+++ dome.py
@@ -1,6 +1,7 @@
-
+import time
 def main():
+    localtime = time.asctime( time.localtime(time.time()) )
     print("Hello, GitHub Actions!")
-
+    print("运行时间为：",localtime)
 if __name__ == "__main__":
     main()
