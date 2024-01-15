d4f923b8bc25b054b2fa83767616865312e0f94f
andyyyds <34959818+andyyds-song@users.noreply.github.com>
2024-01-15 21:20:39 +0800

    Create hhhh.py

---
+++ hhhh.py
@@ -0,0 +1,20 @@
+import sys,os
+
+def main():
+    print("Number of arguments:", len(sys.argv))
+    print("Arguments:", sys.argv)
+    if len(sys.argv) != 3:
+        print("Usage: python demo.py <username> <password>")
+        sys.exit(1)
+
+    # Get the username and password from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    appID = os.environ.get("APP_ID")
+    print(f'id:{appID}')
+
+    # Output the provided username and password
+    print(f"Username: {username}")
+    print(f"Password: {password}")
+if __name__ == "__main__":
+    main()
