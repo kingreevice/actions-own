1c122ce4dcd08b92fbaf364b7c09f96a53cf065b
andyyyds <34959818+andyyds-song@users.noreply.github.com>
2024-01-16 14:42:22 +0800

    Update MT.py

--- MT.py
+++ MT.py
@@ -17,32 +17,5 @@ page_title = driver.title
 
 print("Page Title:", page_title)
 
-# 等待登录窗口加载，可以根据实际情况调整等待时间  
-driver.implicitly_wait(10)
 
-#find_element_by_xpath() 不能使用
-driver.find_element(By.XPATH,'//*[@id="comiis_nv"]/div/div/div/div[2]/a[1]').click()
-
-driver.implicitly_wait(2)
-#根据name定位
-driver.find_element("name", "username").send_keys("andy_123")
-
-driver.implicitly_wait(2)
-#根据name定位
-driver.find_element(By.NAME,"password").send_keys("andy12345678")
-
-
-driver.implicitly_wait(2)
-#根据name定位
-driver.find_element(By.NAME,"loginsubmit").click()#登入
-
-#//*[@id="pt"]/div[1]/a[1]
-driver.implicitly_wait(2)
-#根据文本定位
-driver.find_element(By.LINK_TEXT,"签到").click()#签到
-
-
-time.sleep(5)
-
-# 关闭浏览器
 driver.quit()
