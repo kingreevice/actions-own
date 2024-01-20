08837d130062bd66ba95e3669354a73e9760859b
andyyyds <34959818+andyyds-song@users.noreply.github.com>
2024-01-20 14:57:19 +0800

    Update cesi.py

--- cesi.py
+++ cesi.py
@@ -1,5 +1,28 @@
-import ddddocr
-from util import *
+# from PIL import Image
+# import cv2, ddddocr
+import numpy as np
+from retrying import retry
+from selenium import webdriver
+import os, sys, time, requests
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
+chrome_options.add_argument('window-size=1920x1080') # 指定浏览器分辨率
+chrome_options.add_argument('--disable-gpu') # 谷歌文档提到需要加上这个属性来规避bug
+chrome_options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+
+chromedriver = "/usr/bin/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chrome_options)
+driver.implicitly_wait(10) # 所有的操作都可以最长等待10s
+return driver
+
+
 img_path = os.getcwd() + "/1.png"
 
 ocr = ddddocr.DdddOcr()
